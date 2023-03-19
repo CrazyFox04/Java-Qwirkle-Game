a5e9ca337ec243fe446d0ea0626bb20b661cccdf
Enzo Renard <60904@etu.he2b.be>
2023-03-19 17:03:53 +0100

    Add Tile Javadoc for model package

--- src/main/java/g60904/qwirkle/model/Tile.java
+++ src/main/java/g60904/qwirkle/model/Tile.java
@@ -1,4 +1,9 @@
 package g60904.qwirkle.model;
 
+/**
+ * Tile represent a tile in the game
+ * @param color color of the tile
+ * @param shape shape of the tile
+ */
 public record Tile(Color color, Shape shape) {
 }
