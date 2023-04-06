e722f6a99b8cecb99574a4358e3091b2d5da6a8f
Enzo Renard <60904@etu.he2b.be>
2023-04-06 13:26:00 +0200

    Remove todo

--- src/main/java/g60904/qwirkle/model/Direction.java
+++ src/main/java/g60904/qwirkle/model/Direction.java
@@ -9,7 +9,7 @@ package g60904.qwirkle.model;
 public enum Direction {
     UP(-1, 0), DOWN(1, 0),
     LEFT(0, -1), RIGHT(0, 1);
-    private final int deltaRow;// todo : make it private !!!
+    private final int deltaRow;
     private final int deltaCol;
 
     /**
